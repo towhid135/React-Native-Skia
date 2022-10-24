d7333820251b8331d82db00f6d8f216bd3efb030
towhid135 <towhidulislam252@gmail.com>
2022-10-25 00:21:35 +0600

    accordion added

--- App.js
+++ App.js
@@ -1,28 +1,13 @@
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-import HelloWorld from './components/HelloWorld';
-import Animated3DCard from './components/Animated3DCard';
+import {StyleSheet} from 'react-native';
+import HelloWorld from './components/Skia/HelloWorld';
+import Animated3DCard from './components/Skia/Animated3DCard';
+import Accordion from './screens/Reanimated2/Accordion';
 
 const App = () => {
-  return <Animated3DCard />;
+  return <Accordion />;
 };
 
-const styles = StyleSheet.create({
-  container: {
-    //flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
---
+++ screens/Reanimated2/Accordion.js
@@ -0,0 +1,87 @@
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import Animated, {
+  Layout,
+  FadeIn,
+  FadeOut,
+  FadeInUp,
+  FadeOutUp,
+  FadeOutDown,
+} from 'react-native-reanimated';
+import Rp from '../../constant/Rp';
+
+const Accordion = () => {
+  const [show, setShow] = useState(false);
+  return (
+    <View style={styles.mainContainer}>
+      <Animated.View
+        layout={Layout}
+        entering={FadeInUp.duration(300)}
+        exiting={FadeOutUp.duration(300)}
+        style={{
+          //   borderWidth: 1,
+          //   borderColor: 'black',
+          width: Rp(80),
+          alignItems: 'center',
+        }}>
+        <TouchableOpacity
+          style={styles.touchStyle}
+          onPress={() => setShow(prev => !prev)}
+          activeOpacity={0.7}>
+          <Text style={styles.touchTextStyle}>Touch Here To See Details</Text>
+        </TouchableOpacity>
+        {show && (
+          <Animated.View
+            layout={Layout}
+            entering={FadeInUp.duration(1000)}
+            exiting={FadeOutUp.duration(1000)}
+            style={styles.accordionTextHolderView}>
+            <Text style={styles.accordionText}>
+              The accordion is a graphical control element comprising a
+              vertically stacked list of items, such as labels or thumbnails.
+              Each item can be "expanded" or "collapsed" to reveal the content
+              associated with that item.
+            </Text>
+          </Animated.View>
+        )}
+      </Animated.View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  touchStyle: {
+    // shadowColor: 'black',
+    // shadowOpacity: 0.8,
+    // shadowRadius: 10,
+    // shadowOffset: {width: 0, height: 2},
+    width: '80%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    // borderColor: 'green',
+  },
+  touchTextStyle: {
+    color: 'black',
+    padding: Rp(5),
+    fontWeight: 'bold',
+  },
+  accordionTextHolderView: {
+    width: '80%',
+    backgroundColor: 'gray',
+    borderWidth: 1,
+  },
+  accordionText: {
+    maxWidth: '80%',
+    fontSize: 12,
+    color: 'black',
+    textAlign: 'center',
+  },
+});
+export default Accordion;
